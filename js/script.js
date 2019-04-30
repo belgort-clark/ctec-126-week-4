8e944dde53886c051afb63d8c770c4ea3ecc2be4
Elgort <BElgort@clark.edu>
2019-04-29 17:51:59 -0700

    Updates from class on Monday the 29th

--- js/script.js
+++ js/script.js
@@ -1,2 +1,53 @@
 // script.js
 
+function nomeatballs() {
+    // grab all elements that have class of meatballs
+    // remove identified elements
+    let meatballs = document.getElementsByClassName('meatball');
+    let parent = document.getElementById('sec1');
+
+    // for (let ball = 0; ball < 10; ball++) {
+        parent.removeChild(meatballs);
+    // }
+}
+
+let section1 = document.getElementById('sec1');
+
+for (let i = 0; i < 10; i++) {
+    let newPara = document.createElement('p');
+    newPara.innerText = "I like pizza";
+    newPara.className = 'meatball';
+    section1.appendChild(newPara);    
+}
+
+
+
+// let p = document.getElementsByTagName('a');
+
+// for (let index = 0; index < p.length; index++) {
+//     p[index].className = 'meatball';
+//     p[index].classList.remove('meatball');
+//     p[index].setAttribute('data-bruce','pizza');
+//     p[index].href = 'http://clark.edu';
+//     let thing = p[index].href;
+// }
+
+// let sec1 = document.getElementById('sec1');
+// let p = sec1.getElementsByTagName('p');
+
+// let p = document.getElementById('sec1').getElementsByTagName('p');
+
+// let p = document.getElementsByTagName('li');
+// let p = document.getElementsByTagName('li');
+// let p = document.getElementsByClassName('thing1');
+// let p = document.querySelectorAll('.thing1');
+// let p = document.querySelectorAll('.thing1');
+
+// for (let index = 0; index < p.length; index++) {
+//     // alert(p[index].innerHTML);
+//     p[index].style.padding = '3em';
+//     p[index].style.fontSize = '3em';
+//     p[index].style.textAlign = 'center';
+//     p[index].style.border = '5px solid black';
+//     p[index].style.background = 'pink';
+// }
